8465e493ed35ca40ee2bb8e6c5ae224ec04f44d7
Jimmyzz2 <50456037+Jimmyzz2@users.noreply.github.com>
2019-09-17 00:02:29 -0400

    Update tm_trees.py

--- tm_trees.py
+++ tm_trees.py
@@ -1,28 +1,3 @@
-"""Assignment 2: Trees for Treemap
-
-=== CSC148 Winter 2019 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2019 Bogdan Simion, David Liu, Diane Horton, Jacqueline Smith
-
-=== Module Description ===
-This module contains the basic tree interface required by the treemap
-visualiser. You will both add to the abstract class, and complete a
-concrete implementation of a subclass to represent files and folders on your
-computer's file system.
-"""
-from __future__ import annotations
-import os
-import math
-from typing import List, Tuple, Optional
-from random import randint
-
-
 class TMTree:
     """A TreeMappableTree: a tree that is compatible with the treemap
     visualiser.
