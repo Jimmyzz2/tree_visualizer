ec74fe3e9239f105536306db892ff370bb9fd08e
Jimmyzz2 <50456037+Jimmyzz2@users.noreply.github.com>
2019-09-17 00:03:08 -0400

    Update treemap_visualiser.py

--- treemap_visualiser.py
+++ treemap_visualiser.py
@@ -1,22 +1,3 @@
-"""Assignment 2: Treemap Visualiser
-
-=== CSC148 Winter 2019 ===
-This code is provided solely for the personal and private use of
-students taking the CSC148 course at the University of Toronto.
-Copying for purposes other than this use is expressly prohibited.
-All forms of distribution of this code, whether as given or with
-any changes, are expressly prohibited.
-
-All of the files in this directory and all subdirectories are:
-Copyright (c) 2019 Bogdan Simion, David Liu, Diane Horton, Jacqueline Smith
-
-=== Module Description ===
-This module contains the main program code for the treemap visualisation.
-It is responsible for initializing an instance of TMTree (using a
-concrete subclass, of course), rendering it to the user using pygame,
-and detecting user events like mouse clicks and key presses and responding
-to them.
-"""
 from typing import Optional, Tuple
 import pygame
 from tm_trees import TMTree, FileSystemTree
